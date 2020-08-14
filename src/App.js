11f444d52516a4462a49aa7399cfe7bcf7c23e9f
ClarkTheSup <lknkoqq123@163.com>
2020-08-14 14:08:46 +0800

    navigator

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
+import {Route, BrowserRouter, Switch} from 'react-router-dom';
+import Navigator from './components/Navigator/Navigator';
+import Market from './components/Market/Market';
+
 
 function App() {
   return (
     <div className="App">
-     
+      <BrowserRouter>
+        <Navigator/>
+
+        <Switch>
+          <Route exact path="/market" component={Market}/>
+        </Switch>
+      </BrowserRouter>
+      
     </div>
   );
 }
---
+++ src/components/Navigator/Navigator.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import './Navigator.css';
+
+class Navigator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.super = {}
+    }
+
+    render() {
+        return <div id="navigator">
+            <ul>
+                <li><Link to="/market">
+                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-house" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+                        <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
+                        </svg>
+                        商城
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/order">
+                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-cart2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
+                        </svg>
+                        订单
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/createItem">
+                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-plus" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M8 3.5a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5H4a.5.5 0 0 1 0-1h3.5V4a.5.5 0 0 1 .5-.5z"/>
+                        <path fill-rule="evenodd" d="M7.5 8a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0V8z"/>
+                        </svg>
+                        添加商品
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    }
+}
+
+export default Navigator;
