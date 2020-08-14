510fc6e7a23f28b7320348c99f60d4fa1216f71d
ClarkTheSup <lknkoqq123@163.com>
2020-08-14 17:47:08 +0800

    finish components

--- src/components/Market/Market.js
+++ src/components/Market/Market.js
@@ -7,11 +7,16 @@ import './Market.css';
 class Market extends React.Component {
     constructor(props) {
         super(props);
-        this.super = {}
+        this.state = {items: []}
+        fetch("http://localhost:8080/market", {method: "GET",
+                                                mode: 'cors'})
+            .then(res=>res.json())
+            .then(data => this.setState({items:data}));
     }
 
     render() {
         return <div className="market">
+            { this.state.items.map((key, value) => <Item key={key}/>) }
             <Item/>
             <Item/>
             <Item/>
--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -30,6 +30,27 @@ class Order extends React.Component {
                         <td>瓶</td>
                         <td><button type="button" class="btn btn-secondary">删除</button></td>
                     </tr>
+                    <tr>
+                        <td>可乐1</td>
+                        <td>1</td>
+                        <td>2</td>
+                        <td>瓶</td>
+                        <td><button type="button" class="btn btn-secondary">删除</button></td>
+                    </tr>
+                    <tr>
+                        <td>可乐1</td>
+                        <td>1</td>
+                        <td>2</td>
+                        <td>瓶</td>
+                        <td><button type="button" class="btn btn-secondary">删除</button></td>
+                    </tr>
+                    <tr>
+                        <td>可乐1</td>
+                        <td>1</td>
+                        <td>2</td>
+                        <td>瓶</td>
+                        <td><button type="button" class="btn btn-secondary">删除</button></td>
+                    </tr>
                 </tbody>
             </table>
         </div>
